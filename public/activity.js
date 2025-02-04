7d2b238e9d77e6b08a785bdd43cf2a800126308a
majascale <maja.stojanova@scalefocus.com>
2025-02-04 15:21:00 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -7,27 +7,9 @@ connection.on('initActivity', function( data ) {
     if (data) {
        payload = data;
        //document.getElementById( 'smsText' ).value = JSON.stringify( payload, null, 2 );
-       document.getElementById( 'smsText' ).value = JSON.stringify(payload.arguments.execute.inArguments, null, 2 );
+       document.getElementById( 'smsText' ).value = JSON.stringify(payload.arguments.execute.inArguments[0], null, 2 );
        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-      var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
-       console.log("inArguments:" + inArguments); 
-        $.each(inArguments, function (index, inArgument) {
-          $.each(inArgument, function (key, val) {
-              console.log("TextKey:" + key);
-               console.log("TextVal:" + val);
-               // if (key === 'text') {
-                    //contactKey = val;
-                    //document.getElementById( 'smsText' ).value = val;
-               // }            
-            });
-        }); 
-      console.log("inArguments:" + inArguments);  
+ 
     }
 });
 
