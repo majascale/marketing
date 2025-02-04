8e00afb19078d49203a3eb50aaf195d0012ed6ad
majascale <maja.stojanova@scalefocus.com>
2025-02-04 10:43:40 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 function isViberMessage() {
    var select = document.getElementById('messageType');
-   var value = select.options[select.selectedIndex].value;
-   console.log(value);
+   var option = select.options[select.selectedIndex];
+   document.getElementById('value').value = option.value;
+   document.getElementById('text').value = option.text;
+   console.log(option.value);
 }
