2eebb9054fd2f1c23939207bcfef3accec45e92e
majascale <maja.stojanova@scalefocus.com>
2025-02-04 14:39:19 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -7,7 +7,7 @@ connection.on('initActivity', function( data ) {
     if (data) {
        payload = data;
        //document.getElementById( 'smsText' ).value = JSON.stringify( payload, null, 2 );
-       document.getElementById( 'smsText' ).value = payload['arguments'].execute.inArguments;
+       document.getElementById( 'smsText' ).value = payload.arguments.execute.inArguments[0];
     }
 });
 
