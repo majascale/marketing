4443d7599711eebb3203d7e135b6cc0278280768
majascale <maja.stojanova@scalefocus.com>
2025-02-04 15:31:21 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -7,7 +7,7 @@ connection.on('initActivity', function( data ) {
     if (data) {
        payload = data;
     }
-    document.getElementById( 'smsText' ).value = JSON.stringify(payload.arguments.execute.inArguments[1].val, null, 2 );
+    document.getElementById( 'smsText' ).value = JSON.stringify(payload.arguments.execute.inArguments[1].value, null, 2 );
 });
 
 // Save Sequence
