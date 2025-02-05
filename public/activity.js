bcc5b93e029dd075a5160270063f61fe8f4e8548
majascale <maja.stojanova@scalefocus.com>
2025-02-05 15:42:25 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -23,5 +23,20 @@ connection.on('clickedNext', function() {
   payload.arguments.execute.inArguments[1].text = sms; 
   console.log('Payload-After: ' + JSON.stringify(payload,null,2));  
   connection.trigger('updateActivity', payload);
-  //generate the signature part and update Conf
+  
+  //generate the signature part and update Conf ... in Testing
+  const crypto = require('crypto');
+  // Defining key
+  const secret = '$2y$10$DHkc4KUis70s57hQvBPrfOBlbj.tonKXniTjUBpArymaVqOXxgcn.';
+  const hash = crypto.createHmac('sha512', secret)
+                    .update('{
+    "msisdn": "38977772032",
+    "sc": "1990",
+    "text": "Test message!",
+    "service_id": "2724"
+    }')  // updating data
+                    .digest('hex');           // Encoding to be used
+
+
+console.log(hash); 
 });
