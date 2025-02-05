069ff4918e2c93b7b023a08c8675101c1fb1d5ef
majascale <maja.stojanova@scalefocus.com>
2025-02-05 16:03:16 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -24,19 +24,17 @@ connection.on('clickedNext', function() {
   console.log('Payload-After: ' + JSON.stringify(payload,null,2));  
   connection.trigger('updateActivity', payload);
   
-  //generate the signature part and update Conf ... in Testing
+  // ... in Testing
   const crypto = require('crypto');
-  // Defining key
   const secret = '$2y$10$DHkc4KUis70s57hQvBPrfOBlbj.tonKXniTjUBpArymaVqOXxgcn.';
-  const hash = crypto.createHmac('sha512', secret)
-                    .update('{
+  let hmac = crypto.createHmac('sha512', secret);
+  hmac.update('{
     "msisdn": "38977772032",
     "sc": "1990",
     "text": "Test message!",
     "service_id": "2724"
-    }')  // updating data
-                    .digest('hex');           // Encoding to be used
-
-
-console.log(hash); 
+  }');
+  let hash = hmac.digest('hex');        
+  console.log(hash); 
+    
 });
