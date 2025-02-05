3eb3df73e1c7d736dd7fea7f146c7597f9015928
majascale <maja.stojanova@scalefocus.com>
2025-02-05 10:21:06 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -1,5 +1,6 @@
 var connection = new Postmonger.Session();
 var payload = {};
+
 // Startup Sequence
 connection.trigger('ready');
 
@@ -7,7 +8,6 @@ connection.on('initActivity', function( data ) {
     if (data) {
        payload = data;
     }
-    //document.getElementById( 'smsText' ).value = JSON.stringify(payload.arguments.execute.inArguments[1], null, 2 );
     const json = JSON.stringify(payload.arguments.execute.inArguments[1], null, 2 );
     const obj = JSON.parse(json);
     document.getElementById( 'smsText' ).value = obj.text;
@@ -15,10 +15,9 @@ connection.on('initActivity', function( data ) {
 
 // Save Sequence
 connection.on('clickedNext', function() {
-  //to be checked here Unexpected token 'H', "Hardcoded text1" is not valid JSON var sms = JSON.parse( document.getElementById( 'smsText' ).value );
+
   var sms = document.getElementById( 'smsText' ).value;
   console.log('SMS: ' + sms);
-  //console.log('Payload: ' + JSON.stringify(payload,null,2));
   var jsonObj = JSON.stringify(payload,null,2);
   console.log('jsonObj-Before: ' + jsonObj);  
   payload.arguments.execute.inArguments[1].text = sms; 
