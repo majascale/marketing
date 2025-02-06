b8a7c7adc27a981c5b45dbcf7a63ee842f16a639
majascale <maja.stojanova@scalefocus.com>
2025-02-06 11:33:54 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -25,7 +25,7 @@ connection.on('clickedNext', function() {
   connection.trigger('updateActivity', payload);
   
   // ... in Testing
-  const crypto = require('crypto');
+  const crypto = require('crypto-js');
   const secret = '$2y$10$DHkc4KUis70s57hQvBPrfOBlbj.tonKXniTjUBpArymaVqOXxgcn.';
   let hmac = crypto.createHmac('sha512', secret);
   //let msg = ' "msisdn": "38977772032", "sc": "1990", "text": "Test message!","service_id": "2724" ';
