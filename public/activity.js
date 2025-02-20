7d71384be95a7a83496d09be4ca981c6e67bfed8
majascale <maja.stojanova@scalefocus.com>
2025-02-20 14:21:25 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -8,7 +8,7 @@ connection.on('initActivity', function( data ) {
     if (data) {
        payload = data;
     }
-    console.log('Payload Data: ' + pyaload);
+    console.log('Payload Data: ' + payload);
     //const json = JSON.stringify(payload.arguments.execute.inArguments[1], null, 2 );
     //const obj = JSON.parse(json);
     //document.getElementById( 'smsText' ).value = obj.text;
@@ -22,13 +22,13 @@ connection.on('clickedNext', function() {
   var jsonObj = JSON.stringify(payload,null,2);
   console.log('Payload-Before: ' + jsonObj);  
   payload['metaData'].isConfigured = true;  
-  /*payload['arguments'].execute.inArguments = [
+  payload['arguments'].execute.inArguments = [
             {
                 "type": "sms",
-                "msisdn": "{{Contact.Attribute.Age}}",
-                "text": "{{Contact.Attribute.Country}}",
+                "msisdn": "38977772035",
+                "text": smsText,
             }
-    ];*/  
+    ];  
   //payload.arguments.execute.inArguments[1].text = sms; 
   console.log('Payload-After: ' + JSON.stringify(payload,null,2));  
   connection.trigger('updateActivity', payload);
