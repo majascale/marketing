b5264e1bbdeef5a36f52a3a530dc6c294498fa81
majascale <maja.stojanova@scalefocus.com>
2025-02-20 14:00:31 +0100

    Update activity.js

--- public/activity.js
+++ public/activity.js
@@ -17,36 +17,13 @@ connection.on('initActivity', function( data ) {
 // Save Sequence
 connection.on('clickedNext', function() {
 
-  var sms = document.getElementById( 'smsText' ).value;
-  console.log('SMS: ' + sms);
+  var smsText = document.getElementById( 'smsText' ).value;
+  console.log('smsText is: ' + smsText);
   var jsonObj = JSON.stringify(payload,null,2);
-  console.log('jsonObj-Before: ' + jsonObj);  
-  payload.arguments.execute.inArguments[1].text = sms; 
+  console.log('Payload-Before: ' + jsonObj);  
+  payload['metaData'].isConfigured = true;  
+  //payload.arguments.execute.inArguments[1].text = sms; 
   console.log('Payload-After: ' + JSON.stringify(payload,null,2));  
-  connection.trigger('updateActivity', payload);
-  
-  
-  //const crypto = require("crypto-js");
-  //const secretKey = '$2y$10$DHkc4KUis70s57hQvBPrfOBlbj.tonKXniTjUBpArymaVqOXxgcn.';
-  //let hmac = crypto.createHmac('sha512', secretKey);
-  //let requestBody = 'Test';
-  //let msg = 'GeeksforGeeks';
-  //hmac.update(msg);
-  //let hash = hmac.digest('hex');        
-  //console.log(hash); 
-
- //var myString   = "blablabla Card game bla";
- //var myPassword = "myPassword";
- //var encrypted = CryptoJS.AES.encrypt(myString, myPassword);
-// var hmacTest = CryptoJS.HmacSHA512(requestBody, secretKey).toString();  
- //var hmac = CryptoJS.algo.HMAC.create(CryptoJS.algo.SHA512, secretKey);
- //hmac.update("Test");
- //var hash = hmac.finalize();   
- //console.log('Encrypt is: ' + encrypted); 
- //console.log('Hash is: ' + hash);    
- 
-//var hmacDigest = CryptoJS.HmacSHA512(requestBody, secretKey).toString(CryptoJS.digest);
- 
-     
-    
+  //connection.trigger('updateActivity', payload);
+      
 });
