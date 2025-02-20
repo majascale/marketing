d57db0584ef21a98dabb8f2544de9b963d2d928b
majascale <maja.stojanova@scalefocus.com>
2025-02-20 10:44:38 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-function setViberSectionVisibility() {
+function setSectionVisibility() {
        var select = document.getElementById('messageType');
        var value = select.options[select.selectedIndex].value;
        console.log(value);
