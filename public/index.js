a19cdb6e774404d830738bf1696e7fdbf80614f6
majascale <maja.stojanova@scalefocus.com>
2025-02-21 10:11:51 +0100

    Update index.js

--- public/index.js
+++ public/index.js
@@ -39,3 +39,21 @@ function setViberSectionVisibility(){
            break;                
        }
 }
+function setSectionOnLoad() {
+       var select = document.getElementById('messageType');
+       var value = select.options[select.selectedIndex].value;
+       console.log(value);
+       switch(value) {
+       case 'sms':
+           document.getElementById('smsSection').hidden = false;
+           document.getElementById('viberSection').hidden = true;
+           break;
+       case 'viber':
+           document.getElementById('smsSection').hidden = true;
+           document.getElementById('viberSection').hidden = false;
+           break;
+       default:
+           document.getElementById('smsSection').hidden = true;
+           document.getElementById('viberSection').hidden = true;
+       }
+}
